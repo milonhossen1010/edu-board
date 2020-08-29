9e8679f0a5c6b0d7836833c65f52b665955d3bdd
milonhossen1010 <milonhossen1010@gmail.com>
2020-08-30 01:47:06 +0600

    Database class design

--- app/Support/Database.php
+++ app/Support/Database.php
@@ -1,11 +1,28 @@
 <?php 
+    include_once "../../config.php";
 
-	namespace Edu\board\Support;
+    namespace Edu\board\Support;
+    use PDO;
 
     /**
      * Database Management
      */
-    class Database 
+    abstract class Database 
     {
-        
+        /**
+         * Server information
+         */
+        private $host = HOST;
+        private $user = USER;
+        private $pass = PASS;
+        private $db = DB;
+        private $connection ;
+
+    /**
+     * Database connection
+     */
+        private function connection()
+        {
+           $connection = new PDO("mysql: host=" . $this -> host. ";db_name=" . $this -> db , $user, $pass);
+        }
     }
